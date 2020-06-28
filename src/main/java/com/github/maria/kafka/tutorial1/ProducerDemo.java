54ec986923c1e8e80b27b5520bb9c2eaf13a2795
Maria Vela <maria.vela790@gmail.com>
2020-06-27 22:43:26 -0500

    adding producer and gitignore

---
+++ src/main/java/com/github/maria/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,14 @@
+package com.github.maria.kafka.tutorial1;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+//        create producer properties
+        Properties properties = new Properties();
+
+//        create producer
+
+//        send data
+    }
+}
